1d2b3d54a7919914d57ef74b834eaed9a7f01956
Ben Capper <Bencapper19@gmail.com>
2022-09-13 10:52:51 +0100

    Arrays
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

--- fundamentals.js
+++ fundamentals.js
@@ -1,4 +1,18 @@
-const aCar = { owner : "Ben Capper", type : {
+const aCar = { 
+    owner : "Ben Capper",
+    address: "3 Walkers Lane",
+    previous_owners: [
+        {
+        name: "Pat Bob",
+        address: "2 Low Street",
+        },
+        {
+        name: "Sheila Smith",
+        address: "1 High Street",
+        }
+    ],
+
+type : {
     make: 'Toyota',
     model: 'Corolla',
     cc: 1.8
@@ -11,3 +25,5 @@ console.log(aCar.owner);
 console.log(aCar.owner + ' drives a ' + aCar.type.make)
 console.log('Reg. = ' + aCar.registration.year + '-' + 
             aCar.registration.county + '-' + aCar.registration.number)
+console.log('First owner : ' + aCar.previous_owners[1].name + ' - ' + aCar.previous_owners[1].address )
+
