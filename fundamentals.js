c317c72608dfd88c9c25ce553dad4b6142010b83
Ben Capper <Bencapper19@gmail.com>
2022-09-14 13:24:50 +0100

    Loops
    Signed-off-by: Ben Capper <Bencapper19@gmail.com>

--- fundamentals.js
+++ fundamentals.js
@@ -1,6 +1,7 @@
 const aCar = { 
     owner : "Ben Capper",
     address: "3 Walkers Lane",
+    features : ['Parking assist', 'Alarm', 'Tow-bar'],
     previous_owners: [
         {
         name: "Pat Bob",
@@ -11,15 +12,17 @@ const aCar = {
         address: "1 High Street",
         }
     ],
-
-type : {
-    make: 'Toyota',
-    model: 'Corolla',
-    cc: 1.8
-}, registration : {
-    year: '201',
-    county: 'WD',
-    number: '1058' }};
+    type : {
+        make: 'Toyota',
+        model: 'Corolla',
+        cc: 1.8
+    }, 
+    registration : {
+        year: '201',
+        county: 'WD',
+        number: '1058' 
+    }
+};
 
 console.log(aCar.owner);
 console.log(aCar.owner + ' drives a ' + aCar.type.make)
@@ -27,3 +30,10 @@ console.log('Reg. = ' + aCar.registration.year + '-' +
             aCar.registration.county + '-' + aCar.registration.number)
 console.log('First owner : ' + aCar.previous_owners[1].name + ' - ' + aCar.previous_owners[1].address )
 
+for (let i = 0 ; i < aCar.features.length ; i += 1) {
+    console.log(aCar.features[i]) ;
+}
+
+for (let i = 0 ; i < aCar.previous_owners.length ; i += 1) {
+    console.log(aCar.previous_owners[i]['name']) ;
+}
